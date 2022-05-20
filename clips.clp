3dc8aaed02310eb2a9e5db3dbf5431126028aa8c
Jose Juan <josejuanhernandezgalvez@gmail.com>
2022-05-20 10:59:30 +0100

    Finished

--- clips.clp
+++ clips.clp
@@ -2,6 +2,7 @@
 	(slot zone (type INTEGER))
 	(slot type (type INTEGER))
 	(slot people (type INTEGER))
+	(slot agressors (type INTEGER))
 	(slot guns (type INTEGER))
 )
 
@@ -9,30 +10,49 @@
     (declare (salience 1000))
     (PR (zone ?z) (type 3) (people ?p))
     =>
-    (assert (Alpha Danger in sector ?z with ?p people injured))
+    (assert (Fire in sector ?z with ?p people injured))
 )
 
 (defrule injured_alert
     (PR (zone ?z) (type 2) (people ?p))
     =>
-    (assert (Accident Danger in sector ?z with ?p people injured))
+    (assert (Accident in sector ?z with ?p people injured))
 )
 
 (defrule heist_alert
-    (PR (zone ?z) (type 1) (people ?p))
+    (PR (zone ?z) (type 1) (people ?p) (agressors ?a))
     =>
-    (assert (Heist Danger in sector ?z with ?p people in danger))
+    (assert (Heist in sector ?z with ?p people in danger. There are ?a agressors))
 )
 
 (defrule there_are_guns
-    (PR (guns 1)(type 1)(zone ?z))
+    (PR (guns 1)(type 1))
     =>
     (assert (Robbers armed be precautious))
+)
+
+(defrule send_troops_guns
+    (PR (guns 1)(type 1)(agressors ?a)(people ?p))
+    =>
+    (assert (They have ?p hostages. Cops needed: (+ (* ?a 10) ?p)))
+)
+
+(defrule send_troops_withoutguns
+    (PR (guns 0)(type 1)(agressors ?a)(people ?p))
+    =>
+    (assert (They have ?p hostages. Cops needed: (+ (* ?a 4) ?p)))
+)
+
+(defrule send_fire_fighters
+    (PR (type 3)(people ?p))
+    =>
+    (assert (Fire trucks needed needed: (div ?p 4)))
 )
 
 (defrule injured_people
     (PR (type ?t) (people ?p))
     (test (> ?t 1))
     =>
-    (assert (Ambulances needed: (+ 1 (div ?p 10))))
+    (assert (Ambulances needed: (+ 1 (div ?p 15))))
 )
+
--- main.py
+++ main.py
@@ -1,70 +1,18 @@
 import clips
 import logging
-
-
-class Emergency:
-    def __init__(self, city, position):
-        city.zones[position - 1] = self
-
-
-class Police(Emergency):
-    def __init__(self, city, position):
-        super().__init__(city, position)
-
-
-class Firefigther(Emergency):
-    def __init__(self, city, position):
-        super().__init__(city, position)
-
-
-class Hospital(Emergency):
-    def __init__(self, city, position):
-        super().__init__(city, position)
-
-
-class City:
-    def __init__(self, dimesionX, dimensionY):
-        self.dimensionX = dimesionX
-        self.dimensionY = dimensionY
-        self.area = dimensionY * dimesionX
-        self.zones = [Zone()] * self.area
-
-
-class Zone:
-    def __init__(self):
-        self.id = None
-        self.Police = None
-        self.Hospital = None
-        self.Firefigther = None
-
-    def police(self, police):
-        self.Police = police
-
-    def hospital(self, hospital):
-        self.Hospital = hospital
-
-    def firefigther(self, firefigther):
-        self.Firefigther = firefigther
-
+from random import randint
 
 logging.basicConfig(level=10, format='%(message)s')
 env = clips.Environment()
-
 router = clips.LoggingRouter()
-
-
 env.load('clips.clp')
-
 template = env.find_template('PR')
 
-template.assert_fact(zone=1, type=2, people=3, guns=1)
-env.run()
-for i in env.facts():
-    print(i)
-
-# Reglas a definir en CLIPS
-# 1 - Cómo determinar el nivel de peligro (Qué tenemos en cuenta) - Zona, Si es fuego/atraco/accidente,
-# dimension = grande/media/pequeà, gravedad, cantidad de personas involucradas, mirar las patrullas disponibles
-# y llamar en funcin de eso
-# 2 - Reglas de si sigue el peligro o ha acabado Mirar cada cierto tiempo si el problema o se ha acabado,
-# para que vuelva la patrulla, o sigue el peligro o ha bajado
+for i in range(6):
+    template.assert_fact(zone=randint(0, 20), type=randint(1, 3), people=randint(1, 100), agressors=randint(1, 3),
+                         guns=randint(0, 1))
+    print("-----------------")
+    env.run()
+    for i in env.facts():
+        print(i)
+    env.reset()
